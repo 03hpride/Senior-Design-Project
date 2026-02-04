b3231f226c18ae530aa0b23a99de7c7570181c9a
QuinnCox <qcox@mtech.edu>
2026-02-04 12:05:54 -0900

    Create Login.php
    I created the user interface for login. I am working on functionality to login.

---
+++ 2026Code/Login.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+    <head>
+         <title>Weather Share Notification Sign Up</title>
+        <link rel="stylesheet" href="stylesheet.css">
+    </head>
+    <body>
+        <header>
+            <center>
+            <nav
+                class="navbar navbar-expand-sm navbar-light bg-light"
+            >
+                <div class="container">
+                    <a class="navbar-brand" href="#"><center>Weather Share Notification Center</center></a>
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="collapsibleNavId">
+                        <!-- <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                            <button
+                                type="button"
+                                class="btn btn-primary"
+                                style="margin-right: 10px;"
+                            >
+                                Dashboard
+                            </button>
+                        </ul>
+                        <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                            <button
+                                type="button"
+                                class="btn btn-primary"
+                                style="margin-right: 10px;"
+                            >
+                                Account
+                            </button>
+                       </ul> -->
+                    </div>
+                </div>
+            </nav>
+            </center>
+        </header>
+        <main>
+            <center>
+                <h2 style="padding: 20px;">Login to Your Account</h2>
+            </center>
+
+            <div class="containerbutton-row">
+                <form action="authenticate.php" method="post">
+                    <label for="username">Username:</label><br>
+                    <input type="text" id="username" name="username" required><br><br>
+                    <label for="password">Password:</label><br>
+                    <input type="password" id="password" name="password" required><br><br>
+                    <input type="submit" value="Login">
+                </form>
+            </div>
+        </main>
+        <footer>
+        </footer>
+    </body>
+</html>
